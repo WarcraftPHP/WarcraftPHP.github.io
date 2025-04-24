4b34af1a01b7e4135d2c10001bd12ad9848657be
WarcraftPHP <aleg87773@gmail.com>
2025-04-24 15:00:55 +0500

    Create document_bot_container.py

---
+++ document_bot_container.py
@@ -0,0 +1,18 @@
+import telebot
+API_TOKEN = 'TOKEN'
+bot = telebot.TeleBot(API_TOKEN)
+@bot.message_handler(content_types=['document'])
+def handle_photo(message):
+    user = message.from_user
+    grouptitle = message.chat.title or ""
+    username = user.username or ""
+    doc = message.document
+    file_info = bot.get_file(doc.file_id)
+    downloaded_file = bot.download_file(file_info.file_path)
+    file_path = "YOUR_PATH" + doc.file_name
+    print(message.photo)
+    with open(file_path, 'wb') as f:
+        f.write(downloaded_file)
+        print(f"'{grouptitle}' from @{username} has been saved")
+        print(f"FileSize: {len(downloaded_file)/(1024**2):.2f} bytes")
+bot.polling()
